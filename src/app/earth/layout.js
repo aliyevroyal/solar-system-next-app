7ab8ee0b532a6abe4e9b7e0baf3ab23efe271871
aliyevroyal <royal.alyv@gmail.com>
2024-01-25 18:46:26 +0400

    Initial commit

---
+++ src/app/earth/layout.js
@@ -0,0 +1,10 @@
+import "@/public/css/reset.css";
+
+export const metadata = {
+  title: "Earth Our Home",
+  description: "Blog about our home, Earth",
+};
+
+export default function ClientLayout({ children }) {
+  return children;
+}
---
+++ src/app/mars/layout.js
@@ -0,0 +1,10 @@
+import "@/public/css/reset.css";
+
+export const metadata = {
+  title: "Mars",
+  description: "Blog about Mars",
+};
+
+export default function ClientLayout({ children }) {
+  return children;
+}
